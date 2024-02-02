770a1fdd2ee3338502561768c16cbdca9b15f4d7
Mike Ngetich <125536368+kMike05@users.noreply.github.com>
2024-02-02 23:42:23 +0300

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,6 @@
+color = input ("Enter a color ")
+noun = input ("Enter a plural noun ")
+celebrity = input ("Enter a celebrity name ")
+print("Roses are "+color+"")
+print(""+noun+" are blue")
+print("I love "+celebrity+"")
